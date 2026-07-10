714e64c21241435d31b6aa529c88a3aaff5f794e
juffalx <juffalx@gmail.com>
2026-07-11 02:57:44 +0300

    final edited TeleBirr Tip Calculator

--- Module-01/day02/tip_calculator.py
+++ Module-01/day02/tip_calculator.py
@@ -2,25 +2,24 @@
 
 #step 1 , store a bill total and number of people
 
-total_bill = 10000
-people = 2
+total_bill = 9300
+people = 3
 
-#step 2 write functin split_bill
+#step 2 and 3 write functin split_bill and add total and tip_rate parameter and return per-person values
+
 def split_bill(total, tip_rate=0.2):
     total_plus_tip = total + (total * tip_rate)
     per_person = total_plus_tip / people
-
-    per_person = int(per_person)
     return per_person
     
 
-#step 4
+#step 4 loop over a list of name and print it with each name with shared bills
 
 
-friends = ["mame", "ela"] #list 
+friends = ["Mohammed", "Amir", "Eliyas"] #list for 3 person name of my friend
 
 
 
 for name in friends:
-    print(" Share is {5 + 5 } birrr")
+    print(f"{name} Share bill is {split_bill(total_bill) } Birr  ")
 
