224df3c6e4d074e87f6e3ecc90f0e432bbdef3b9
juffalx <juffalx@gmail.com>
2026-07-11 23:20:51 +0300

    update count even numbers for filter option for even numbers

--- Module-01/day02/daily_practice_solve_problem/count_even_numbers.py
+++ Module-01/day02/daily_practice_solve_problem/count_even_numbers.py
@@ -10,9 +10,19 @@ def count_evens(numbers):
 
     return count
 
+def filter_evens(numbers):
+    temp_evens = []
+    for num in numbers:
+        if num % 2 == 0:
+            temp_evens.append(num)
+    
+    return temp_evens
+
+
+
 
 user_input = input("Enter Number each one separated by spaces: ")
-
+print_output_user_input = input("if you want filter the even number press y or for skip click any key")
 numbers = []
 
 for value in user_input.split():
@@ -21,3 +31,10 @@ for value in user_input.split():
 result = count_evens(numbers)
 
 print("total even numbers:", result)
+
+
+if print_output_user_input == "y" or "Y":
+    status = True
+
+if status:
+    print(f"value is here {filter_evens(numbers)}")
