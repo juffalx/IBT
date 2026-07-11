d83651cf89920bced2fc08fc605f91a8a1d98a17
juffalx <juffalx@gmail.com>
2026-07-11 10:45:19 +0300

    Day 2 Problem Solve Task 5 Counting passing Student

--- Module-01/day02/daily_practice_solve_problem/count_passed_student.py
+++ Module-01/day02/daily_practice_solve_problem/count_passed_student.py
@@ -13,7 +13,7 @@ for i in grade_input.strip().split(","):
     
     grades.append(i)
 
-#print grades
+#print grade's
 print(grades)
 passed_Student = count_passed_students(grades)
 print(passed_Student)
