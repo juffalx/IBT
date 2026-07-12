b09d74437fec3800ff35ce30e05cf2d37d25d54a
juffalx <juffalx@gmail.com>
2026-07-12 19:18:55 +0300

    problem 2 reverse_string daily practice 2nd_updated

--- Module-01/day02/daily_practice_solve_problem/reverse_string.py
+++ Module-01/day02/daily_practice_solve_problem/reverse_string.py
@@ -1,12 +1,11 @@
 #reverse string words 
 
 def reverse_word(text):
-    revese_text = ""
+    reverse_text = ""
     for character in text:
-        revese_text = character + revese_text
+        reverse_text = character + reverse_text
 
-    return str(revese_text)
+    return reverse_text
 
 user_text = input("Enter text: ")
 print(f"reverse of {user_text} is {reverse_word(user_text)}")
-
