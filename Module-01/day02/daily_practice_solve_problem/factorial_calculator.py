913e6c3dad0a069c9dd12e50faf9e3558d8758e7
juffalx <juffalx@gmail.com>
2026-07-12 20:32:02 +0300

    updates factorial calculator code for negative input's

--- Module-01/day02/daily_practice_solve_problem/factorial_calculator.py
+++ Module-01/day02/daily_practice_solve_problem/factorial_calculator.py
@@ -3,6 +3,9 @@
 user_number = int(input("please Enter Possitive Intiger number: "))
 
 def calculate_factorial(number):
+    if (number <= 0):
+        return "Factorial is not defined for negative numbers."
+    
     sum = 1
     for num in range(1, number + 1):
         sum = sum * num
