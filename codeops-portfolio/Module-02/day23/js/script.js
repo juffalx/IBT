9e73002d6881e4a42fce7d07f8c65c28187074d2
juffalx <juffalx@gmail.com>
2026-08-19 10:52:51 +0300

    future:render fetch(API)

--- codeops-portfolio/Module-02/day23/js/script.js
+++ codeops-portfolio/Module-02/day23/js/script.js
@@ -28,8 +28,6 @@ async function  loadMenu(){
 loadMenu()
 // loadMenu() for check if loadmenu is working
 
-
-
 function render(){
     // const term = state.search.toLowerCase();
     // search wst yalewn wede lower case qeyrot term lay yasqemtewal
