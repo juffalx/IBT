8f8ff936492965500e1022e60e48609f345f5969
juffalx <juffalx@gmail.com>
2026-08-25 20:13:38 +0300

    day 26 Exercise React Future: App , Header

---
+++ codeops-portfolio/Module-03/day26/exercise/addis-eats/src/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+
+function Header(){
+    return(
+        <>
+        <Heads>
+            <h1>Tessst</h1>
+        </Heads>
+        </>
+    )
+}
+export default Header;
