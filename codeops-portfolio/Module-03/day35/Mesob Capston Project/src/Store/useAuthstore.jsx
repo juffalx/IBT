ec854937c6598cfb49d3b09fe840b4528f811446
juffalx <juffalx@gmail.com>
2026-09-24 17:29:29 +0300

    feat: setup Zustand store for cart and auth with persistence

---
+++ codeops-portfolio/Module-03/day35/Mesob Capston Project/src/Store/useAuthstore.jsx
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+export const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      isAuthenticated: false,
+      login: (userData) => set({ user: userData, isAuthenticated: true }),
+      logout: () => set({ user: null, isAuthenticated: false }),
+    }),
+    { name: 'addis-eats-auth' }
+  )
+);
